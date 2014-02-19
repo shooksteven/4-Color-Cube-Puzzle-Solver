2e33166ae988458c1764deb45339447e7a8f2c36
Steven Shook <shooksteven@gmail.com>
2014-02-19 13:48:49 -0500

    Cubes initiated with fake data

--- TopDownBruteForce.py
+++ TopDownBruteForce.py
@@ -12,27 +12,55 @@ __author__ = 'sshook'
 class Colors:
     blue, green, red, yellow = range(4)
 
+'''
+class side:
+    def __init__(self):
 
-class Die:
-    '''
-        Below is a drawing of a rolled out die
-            [Front]
-      [Left][Bottom][Right]
-            [Back]
-            [Top]
+            Below is a drawing of a rolled out die
+                [Front]
+          [Left][Bottom][Right]
+                [Back]
+                [Top]
 
-        Dies are represented by an array with the following format
-        [front, left, bottom, right, back, top]
-    '''
-    sides[4] = -1
-    front = -1
-    left = -1
-    bottom = -1
-    right = -1
-    back = -1
-    top = -1
+            Dies are represented by an array with the following format
+            [front, left, bottom, right, back, top]
 
 
-print Colors.red
+        #self.sides[6] = -1
+        self.color = -1
+        self.top = None
+        self.bottom = None
+        self.left = None
+        self.right = None
+'''
 
-#hi amber!
+class cube:
+    def __init__(self, front, left, bottom, right, back, top):
+        self.top = top
+        self.bottom = bottom
+        self.left = left
+        self.right = right
+        self.front = front
+        self.back = back
+
+def printCube(cube):
+    print "\t[", cube.front , ", " , cube.left , ", " , cube.bottom , ", " , cube.right , ", " , cube.back , ", " , cube.top , "]"
+
+
+#initialize the cubes
+cubes = []
+cubes.append(cube(Colors.red, Colors.green, Colors.blue, Colors.yellow, Colors.green, Colors.red))
+cubes.append(cube(Colors.green, Colors.green, Colors.yellow, Colors.red, Colors.yellow, Colors.blue))
+cubes.append(cube(Colors.red, Colors.blue, Colors.blue, Colors.yellow, Colors.yellow, Colors.red))
+cubes.append(cube(Colors.yellow, Colors.blue, Colors.blue, Colors.green, Colors.green, Colors.green))
+
+print("\nAll cubes")
+for cube in cubes:
+    printCube(cube)
+
+
+print("\nIterations of cube 1")
+printCube(cubes[0])
+
+
+
